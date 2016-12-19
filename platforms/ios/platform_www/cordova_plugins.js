a55586c49de7efe6b8306cb83d87556815221c94
gabrielstuff <gabriel@soixantecircuits.fr>
2016-12-19 10:12:57 +0100

    mac os testing

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -15,13 +15,23 @@ module.exports = [
         "clobbers": [
             "cordova.logger"
         ]
+    },
+    {
+        "id": "cordova-plugin-zeroconf.ZeroConf",
+        "file": "plugins/cordova-plugin-zeroconf/www/zeroconf.js",
+        "pluginId": "cordova-plugin-zeroconf",
+        "clobbers": [
+            "cordova.plugins.zeroconf"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-plugin-add-swift-support": "1.6.0",
+    "cordova-plugin-console": "1.0.5",
     "cordova-plugin-whitelist": "1.3.1",
-    "cordova-plugin-console": "1.0.5"
+    "cordova-plugin-zeroconf": "1.2.1"
 };
 // BOTTOM OF METADATA
 });

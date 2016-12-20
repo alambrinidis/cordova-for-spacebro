b93b16263fae20b61d83ad33a5f6727c27abac25
lambri_a <alexandre.lambrinidis@epitech.eu>
2016-12-20 16:33:31 +0100

    safe push

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-zeroconf/www/zeroconf.js",
+        "id": "cordova-plugin-zeroconf.ZeroConf",
+        "pluginId": "cordova-plugin-zeroconf",
+        "clobbers": [
+            "cordova.plugins.zeroconf"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1"
+    "cordova-plugin-whitelist": "1.3.1",
+    "cordova-plugin-add-swift-support": "1.6.0",
+    "cordova-plugin-zeroconf": "1.2.1"
 }
 // BOTTOM OF METADATA
 });

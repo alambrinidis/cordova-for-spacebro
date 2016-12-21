9bcb3f2b51dd3dd01a3289ca728c2c3fd9832a88
lambri_a <alexandre.lambrinidis@epitech.eu>
2016-12-21 14:12:05 +0100

    README edited

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -12,8 +12,9 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1",
     "cordova-plugin-add-swift-support": "1.6.0",
+    "cordova-plugin-console": "1.0.5",
+    "cordova-plugin-whitelist": "1.3.1",
     "cordova-plugin-zeroconf": "1.2.1"
 }
 // BOTTOM OF METADATA
